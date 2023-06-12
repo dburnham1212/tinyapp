d04d9d52626f5b0e05afc12662a496a3de0df352
dburnham1212 <dburnham1212@gmail.com>
2023-06-12 17:31:33 -0400

    added gereateRandomString to express_server.js to print out a random 6 character string

--- express_server.js
+++ express_server.js
@@ -2,6 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// function to genereate a random string based off of the length parameter
+function generateRandomString(length) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'//usable characters
+  let count = 0;
+  let randomString = '';
+  // loop throught the available characters and append each to randomString
+  while(count < length){
+    let charPosition = Math.round(Math.random() * (chars.length - 1));
+    randomString = `${randomString}${chars.charAt(charPosition)}`
+    count ++;
+  }
+  return randomString;
+}
+
+console.log(generateRandomString(6));
+
 // Set ejs as the view engine
 app.set("view engine", "ejs");
 
@@ -9,6 +25,13 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
