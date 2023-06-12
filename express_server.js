019adc7ad2638cb7b396f3e16357e12869fb1a8b
dburnham1212 <dburnham1212@gmail.com>
2023-06-12 17:11:57 -0400

    Added template to create new shortened URLS using urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -19,6 +19,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
@@ -26,10 +30,6 @@ app.get("/urls/:id", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
